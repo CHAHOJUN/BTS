9e3afa79e2efe184d6893e663bc945ea22794039
CHAHOJUN <138743418+CHAHOJUN@users.noreply.github.com>
2023-11-28 10:43:03 +0900

    chj
    chj

--- src/main/java/com/example/board/config/CustomAuthFailureHandler.java
+++ src/main/java/com/example/board/config/CustomAuthFailureHandler.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
+/////////////////
 @Component
 @Slf4j
 public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler{
